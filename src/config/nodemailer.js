06cea9fbd378979fd66d090f40f9d62ea3e99bd5
Cristhian <cristhian.panora@gmail.com>
2024-01-15 16:23:56 -0500

    Sprint 1

---
+++ src/config/nodemailer.js
@@ -0,0 +1,55 @@
+import nodemailer from "nodemailer"
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: process.env.HOST_MAILTRAP,
+    port: process.env.PORT_MAILTRAP,
+    auth: {
+        user: process.env.USER_MAILTRAP,
+        pass: process.env.PASS_MAILTRAP,
+    }
+});
+
+const sendMailToUser = (userMail, token) => {
+
+    let mailOptions = {
+        from: process.env.USER_MAILTRAP,
+        to: userMail,
+        subject: "Verifica tu cuenta",
+        html: `<p>Hola, haz clic <a href="${process.env.URL_BACKEND}confirmar/${encodeURIComponent(token)}">aquí</a> para confirmar tu cuenta.</p>`
+    };
+    
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Correo enviado: ' + info.response);
+        }
+    });
+};
+
+// send mail with defined transport object
+const sendMailToRecoveryPassword = async(userMail,token)=>{
+    let info = await transporter.sendMail({
+    from: 'admin@vet.com',
+    to: userMail,
+    subject: "Correo para reestablecer tu contraseña",
+    html: `
+    <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
+    <hr>
+    <a href=${process.env.URL_BACKEND}recuperar-password/${token}>Clic para reestablecer tu contraseña</a>
+    <hr>
+    <footer>Grandote te da la Bienvenida!</footer>
+    `
+    });
+    console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
+}
+
+export {
+    sendMailToUser,
+    sendMailToRecoveryPassword
+}
--- src/routers/veterinario_routes.js
+++ src/routers/veterinario_routes.js
@@ -3,10 +3,12 @@ import {Router} from 'express'
 // crear una instancia de Router()
 const router = Router()
 
+
+import verificarAutenticacion from '../middlewares/autenticacion.js'
+import { validacionVeterinario } from '../middlewares/validacionVeterinario.js';
 // importar metodos del controlados
 import {
     login,
-    perfil,
     registro,
     confirmEmail,
     listarVeterinarios,
@@ -22,7 +24,7 @@ import {
 // ruta para el login
 router.post("/login", login);
 // ruta para el registro
-router.post("/registro", registro);
+router.post("/registro", validacionVeterinario,registro);
 // ruta para confirmar el token
 router.get("/confirmar/:token", confirmEmail);
 // ruta para listar veterinarios
@@ -33,14 +35,15 @@ router.get("/recuperar-password", recuperarPassword);
 router.get("/recuperar-password/:token", comprobarTokenPasword);
 // ruta para generar el nuevo password
 router.post("/nuevo-password/:token", nuevoPassword);
+
 // ruta para mostrar el perfil del usuario
-router.get("/perfil", perfil);
+// router.get("/perfil",verificarAutenticacion, perfil);
 // ruta para actualizar el password 
-router.put('/veterinario/actualizarpassword',actualizarPassword)
+router.put('/veterinario/actualizarpassword',verificarAutenticacion,actualizarPassword)
 // ruta para mostrar el detalle de un veteriario
-router.get("/veterinario/:id", detalleVeterinario);
+router.get("/veterinario/:id", verificarAutenticacion,detalleVeterinario);
 // ruta para actualizar el perfil
-router.put("/veterinario/:id", actualizarPerfil);
+router.put("/veterinario/:id", verificarAutenticacion,actualizarPerfil);
 
 
 export default router
